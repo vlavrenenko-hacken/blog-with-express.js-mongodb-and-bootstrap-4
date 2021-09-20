59c424c44fa209fc8ee57c8e4f8eb6e819503930
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-20 18:52:52 +0300

    Add password encryption

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 
@@ -7,6 +8,13 @@ const UserSchema = new Schema({
     password:String
 })
 
+UserSchema.pre('save', function(next){
+    const user = this
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash
+        next()
+    })
+})
 
 const User = mongoose.model('User', UserSchema);
 
