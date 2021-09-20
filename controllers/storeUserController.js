daf6d042feafd6d3594e2d2d24f8fdc005bdfcf5
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-20 19:38:00 +0300

    Add try catch

--- controllers/storeUserController.js
+++ controllers/storeUserController.js
@@ -1,6 +1,11 @@
 const User = require('../models/User');
 
 module.exports = async(req, res)=>{
-    await User.create(req.body);
-    res.redirect('/');
+    try{
+        await User.create(req.body);
+        res.redirect('/');
+    }catch(err){
+      res.redirect('/users/new')
+    }
+
 }
