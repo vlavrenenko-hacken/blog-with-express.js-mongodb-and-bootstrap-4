4913fa0f47c55e4273207d76886d37efb0d4b1b3
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-20 13:46:49 +0300

    Add BlogPost model to store blogposts

---
+++ models/BlogPost.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const BlogPostSchema = new Schema({
+    title:String,
+    body:String,
+    username:String,
+    datePosted:{
+        type:Date,
+        default:new Date()
+    },
+    image:String
+})
+
+const BlogPost = mongoose.model('post', BlogPostSchema);
+module.exports = BlogPost;
+
+
+
