2ad28ab9c175ddd84e54802fcf1b858a6194914d
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-20 15:45:42 +0300

    Add controllers for every single route

---
+++ controllers/fullTextSearchController.js
@@ -0,0 +1,9 @@
+const BlogPost = require("./models/BlogPost");
+
+module.exports = (req, res)=>{
+    const query = req.query.data
+    BlogPost.find({$or:[{title:{'$regex':query}}, {body:{'$regex':query}}]}, (err, posts)=> {
+        res.render('index', {posts: posts});
+        console.log(err);
+    })}
+
---
+++ controllers/storePostController.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const BlogPost = require("./models/BlogPost");
+
+module.exports = (req, res)=>{
+    if (!req.files) {
+        res.send("File was not found");
+        return;
+    }
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname,'public/img', image.name), async (error) => {
+        await BlogPost.create({...req.body, image: `/img/${image.name}`});
+        console.log(path.resolve(__dirname, 'public/img', image.name));
+        console.log('-------------------------------------------------------------------------------------------');
+        res.redirect('/');
+    })}
+
+
