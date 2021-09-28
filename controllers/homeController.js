8c16db0ff68169efadaed013fc3d0df01a4fae8e
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-29 01:52:17 +0300

    Add pagination

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,12 @@
 const BlogPost = require("../models/BlogPost");
 
+global.counter = 1;
+
 module.exports=async(req, res)=>{
+    global.counter=1;
     const blogPosts = await BlogPost.find({});
-    console.log(req.session)
-    res.render('index', {posts:blogPosts});
+    const totalPosts = blogPosts?blogPosts.length:0;
+    const filteredPosts = blogPosts.slice(0, 4);
+
+    res.render('index', {posts:filteredPosts, length:totalPosts});
 }
