2cb0f21200c4e4dba9de8de2d2edf50b9f217133
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-29 16:32:54 +0300

    Get user credentials from flash area and pass them into register view

--- controllers/newUserController.js
+++ controllers/newUserController.js
@@ -1,5 +1,15 @@
 module.exports = (req, res)=>{
-    res.render('register', {
-        errors: req.flash('validationErrors')
-    } )
+    let username = ""
+    let password = ""
+    const data = req.flash('data')[0];
+    if(typeof data != "undefined"){
+        username = data.username
+        password = data.password
+    }
+    res.render('register',{
+        errors: req.flash('validationErrors'),
+        username: username,
+        password: password
+    })
 }
+
