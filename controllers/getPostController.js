edc21ff9ad9d0855317e13696125d3cb0a45978a
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-29 20:04:06 +0300

    Add a reference between blogposts and users

--- controllers/getPostController.js
+++ controllers/getPostController.js
@@ -1,7 +1,7 @@
 const BlogPost = require("../models/BlogPost");
 
 module.exports = async(req, res)=>{
-    const post = await BlogPost.findById(req.params.id);
+    const post = await BlogPost.findById(req.params.id).populate('userid');
     res.render('post', {
         blogPost:post
     })
