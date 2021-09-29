21a18eafd61dad200c4a3a5caee8069c4ab5a59e
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-29 19:09:07 +0300

    Pass errorList and user's credentials into the login view

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,15 @@
 module.exports = (req, res) =>{
-    res.render('login')
+    let username = ""
+    let password = ""
+    const data = req.flash('loginData')[0];
+    if(typeof data != "undefined"){
+        username = data.username;
+        password = data.password;
+    }
+    const errorsList = [req.flash('loginError')[0]?.error];
+    res.render('login', {
+        errors:errorsList,
+        username:username,
+        password:password
+    })
 }
