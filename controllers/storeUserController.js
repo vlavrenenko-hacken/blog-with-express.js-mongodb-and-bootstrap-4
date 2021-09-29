e6123e9824415e8f1d16e1ca12005fcc0842ffaa
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2021-09-29 16:15:18 +0300

    add functionality to store validationErrors parameter in the flash section

--- controllers/storeUserController.js
+++ controllers/storeUserController.js
@@ -5,7 +5,7 @@ module.exports = async(req, res)=>{
         await User.create(req.body);
         res.redirect('/users/login');
     }catch(error){
-        req.session.validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+        req.flash('validationErrors', Object.keys(error.errors).map(key => error.errors[key].message))
         res.redirect('/users/new');
     }
 }
